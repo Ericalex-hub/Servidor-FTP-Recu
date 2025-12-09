296231de189351797b169214a4dafa42baecfdcb
Ericalex-hub <ealdsan503@g.educaand.es>
2025-12-09 18:42:07 +0100

    Modificación del archivo named, confirmación y copia de seguridad

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,5 @@
 sudo apt update
 sudo apt install bind9 bind9utils bind9-doc -y
+sudo nano /etc/default/named
+sudo nano /etc/bind/named.conf
+sudo cp /etc/bind/named.conf.options /etc/bind/named.conf.options.backup
