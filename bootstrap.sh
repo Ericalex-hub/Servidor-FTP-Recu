f2a1f472ee959ddfd2930ea1fd4d0d50caddc3eb
Ericalex-hub <ealdsan503@g.educaand.es>
2025-12-09 18:52:16 +0100

    Modificación del archivo named.conf.options, verificación de sintaxis y reinicio y verificación del estado de named

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,5 @@ sudo apt install bind9 bind9utils bind9-doc -y
 sudo nano /etc/default/named
 sudo nano /etc/bind/named.conf
 sudo cp /etc/bind/named.conf.options /etc/bind/named.conf.options.backup
+sudo nano /etc/bind/named.conf.options
+sudo named-checkconf /etc/bind/named.conf.options
